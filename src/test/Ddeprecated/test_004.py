47e4a1914a576d8feb34d8ad7d083142c8a8eb06
gabrielcrooke <gabriecrooke@gmai.com>
2022-11-23 09:10:10 +0800

    Corregido

--- src/test/Ddeprecated/test_004.py
+++ src/test/Ddeprecated/test_004.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 import time
 from selenium.webdriver.support.ui import Select
 
+
 class test_004(unittest.TestCase):
 
     def setUp(self):
@@ -12,12 +13,12 @@ class test_004(unittest.TestCase):
     def test_004(self):
         self.driver.get('https://www.correoargentino.com.ar/')
 
-        Select(self.driver.find_element('xpath', '//*[@id="block-block-15"]/div/div/div/div[2]/select')).select_by_visible_text('DNI')
-
+        Select(self.driver.find_element('xpath',
+                                        '//*[@id="block-block-15"]/div/div/div/div[2]/select')).select_by_visible_text(
+            'DNI')
 
     def tearDown(self):
         time.sleep(15)
-
 
     if __name__ == '__main__':
         unittest.main()
